1e91df07c33d59aa305be718d560962ffd740afb
Mahdavi Riccardo <45188583+MadaBebop@users.noreply.github.com>
2024-03-21 18:38:54 +0100

    Delete react-redux directory

--- react-redux/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from 'react-dom/client';
-import "./styles-invoice.css";
-import App from "./App";
-
-
-const domNode = document.getElementById("application-content");
-const root = createRoot(domNode);
-root.render(<App/>);
